aa07153ed8d58963c5e19432aa5bc5fcfbfa0070
Dhruvvi <dhruvigupta9@gmail.com>
2026-06-27 16:31:56 +0530

    first commit - project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home()
+{
+  return(
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4x1 font-bold">BookIt</h1>
+        <p className="text-gray-500 mt-2">Your scheduling app</p>
+      </div>
+    </main>
+  )
+}
