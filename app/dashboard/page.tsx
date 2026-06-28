2f7a894de24eaf055ac620b5cfc28fd89217b952
Dhruvvi <dhruvigupta9@gmail.com>
2026-06-28 14:54:47 +0530

    added clerk authentication and dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+import { UserButton } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+export default async function DashboardPage() {
+    const {userId} = await auth()
+    if(!userId) redirect('/sign-in')
+
+
+return (
+    <main className="min-h-screen p-8">
+
+        <div className="flex items-center justify-between mb-8">
+            <h1 className="text-3x1 font-bold">Dashboard</h1>
+            <UserButton />
+        </div>
+        <p  className="text-gray-500">Welcome to your BookIt dashboard!</p>
+    
+    </main>
+)
+}
---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import {SignUp} from '@clerk/nextjs'
+export  default function SignUpPage() {
+    return (
+        <main className="min-h-screen flex items-center justify-center">
+            <SignUp />
+        </main>
+    )
+}
